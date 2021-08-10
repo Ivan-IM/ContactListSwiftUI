bf5d39d4e1d7993cced8dffbb23d9b39a42ddea2
iMarishin <move_up@icloud.com>
2021-08-10 09:26:12 +0500

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Иван Маришин on 10.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
